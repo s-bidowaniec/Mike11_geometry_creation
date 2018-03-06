7d3897bb3aa441d24221e03184a145a8ce0346f5
Ciurski <s.bidowaniec@gmail.com>
2018-03-06 20:53:32 +0100

    tworzenie klass linkow z danymi v0.1, brak odpowiedniego nazewnictwa

--- classes_lib.py
+++ classes_lib.py
@@ -13,14 +13,44 @@ class XS(object):
             except:
                 print(element)
         self.max_left = max(elev_points[0:5])
+        self.mean_left = float(self.max_left) / 5
         self.max_right = max(elev_points[-6:-1])
+        self.mean_right = float(self.max_right) / 5
     pass
 
 class link(object):
     def __init__(self, object1, object2):
+        self.object1 = object1
+        self.object2 = object2
         self.pkt = []
         self.river1 = object1.river_code
         self.chain1 = object1.km
         self.river2 = object2.river_code
         self.chain2 = object2.km
         self.rzad = 0
+        self.kolej = 0
+        self.main_chan = "Riv"
+        self.main_km = 0
+        self.main_site = "C"
+        self.topo = "Topo"
+
+    def data_definition(self):
+        if self.rzad == 0:
+            print("brak przypisania")
+            print(self.river1, self.chain1, self.river2, self.chain2)
+        else:
+            if self.kolej == 1:
+                self.definitions = ["KP_"+str(self.main_chan)+"_"+str(self.main_km)+"_"+self.main_site, self.topo,0,5,0,10000,1]
+                self.connections = [self.object1.river_code, self.object1.km, self.object2.river_code, self.object2.km]
+                self.points = [float(self.object1.left[0])-2, float(self.object1.left[1])-2, float(self.object1.left[0])+2, float(self.object1.left[1])+2]
+                h_elev = float(max(self.object1.max_left, self.object2.max_right))
+                self.geometry = [h_elev, h_elev-1]
+                self.cross_section = [[0,0],[0.1, float(self.object1.len)], [3, float(self.object1.len)]]
+            elif self.kolej == 2:
+                self.definitions = ["KP_"+str(self.main_chan)+"_"+str(self.main_km)+"_"+self.main_site, self.topo,0,5,0,10000,1]
+                self.connections = [self.object2.river_code, self.object2.km, self.object1.river_code, self.object1.km]
+                self.points = [float(self.object1.left[0])-2, float(self.object1.left[1])-2, float(self.object1.left[0])+2, float(self.object1.left[1])+2]
+                h_elev = float(max(self.object1.max_left, self.object2.max_right))
+                self.geometry = [h_elev, h_elev-1]
+                self.cross_section = [[0,0],[0.1, float(self.object1.len)], [3, float(self.object1.len)]]
+            pass
--- read_raw.py
+++ read_raw.py
@@ -52,7 +52,7 @@ for line in lines:
         pass
     row +=1
 
-# przypisacie left right kordynaty i max
+# przypisanie przekroją wspolrzednych left i right oraz max z na krancach
 for i in range(len(XS_dat)):
     XS_dat[i].kordy()
     if i == 0 or i == len(XS_dat)-1:
@@ -60,12 +60,87 @@ for i in range(len(XS_dat)):
     elif i > 0 and i < len(XS_dat):
         XS_dat[i].len = abs((XS_dat[i-1].km-XS_dat[i+1].km)/2)
 
+# wykrycie polaczen przekroii od lewej do prawej, stworzenie listy obiektow typu link
 linki = []
 for object1 in XS_dat:
     lewa = object1.left
     for object2 in XS_dat:
-        if abs(float(lewa[0]) - float(object2.right[0]))<10 and abs(float(lewa[1]) - float(object2.right[1]))<10:
+        if abs(float(lewa[0]) - float(object2.right[0]))<10 and abs(float(lewa[1]) - float(object2.right[1]))<10 and object1.river_code != object2.river_code:
             linki.append(link(object1, object2))
+licz_lin = len(linki)
+defined = 0
+#Nadanie parametrow przekroja
+for element in linki:
+    if "TZ_" not in element.river1 and "TZ_" in element.river2:
+        #print(element.river1, element.chain1, element.river2, element.chain2)
+        element.rzad = 1
+        element.kolej = 1
+        defined +=1
+    elif "TZ_" in element.river1 and "TZ_" not in element.river2:
+        element.rzad = 1
+        element.kolej = 2
+        defined += 1
+    elif "TZ_" not in element.river1 and "TZ_" not in element.river2:
+        #print(element.river1,element.chain1, element.river2, element.chain2)
+        if element.object1.mean_left > element.object2.mean_right:
+            element.rzad = 1
+            element.kolej = 1
+            defined += 1
+        else:
+            element.rzad = 1
+            element.kolej = 2
+            defined += 1
+#print(len(linki))
+#print(defined)
+rzad = 1
+safety = 0
+while len(linki) > defined and safety < 10:
+    for element in linki:
+        if element.rzad == rzad:
+            if element.kolej == 1:
+                for element2 in linki:
+                    if element.river2 == element2.river1 and element.chain2 == element2.chain1 and element2.rzad == 0:
+                        element2.rzad = rzad+1
+                        element2.kolej = 1
+                        defined += 1
+                        #print(element2.river1, element2.chain1, element2.river2, element2.chain2)
+                    elif element.river2 == element2.river1 and element.chain2 == element2.chain1 and element2.rzad == rzad:
+
+                        if element2.object1.mean_left > element2.object2.mean_right:
+                            element2.rzad = rzad+1
+                            element2.kolej = 1
+
+                        else:
+                            element2.rzad = rzad+1
+                            element2.kolej = 2
+
+            elif element.kolej == 2:
+                for element2 in linki:
+                    if element.river1 == element2.river2 and element.chain1 == element2.chain2 and element2.rzad == 0:
+                        element2.rzad = rzad+1
+                        element2.kolej = 2
+                        defined += 1
+                        #print(element2.river1, element2.chain1, element2.river2, element2.chain2)
+                    elif element.river1 == element2.river2 and element.chain1 == element2.chain2 and element2.rzad == rzad:
+                        if element2.object1.mean_left > element2.object2.mean_right:
+                            element2.rzad = rzad+1
+                            element2.kolej = 1
+
+                        else:
+                            element2.rzad = rzad+1
+                            element2.kolej = 2
+
+    rzad += 1
+    safety += 1
+    print(len(linki))
+    print(defined)
+for element in linki:
+    # self.definitions = ["KP_"+str(self.main_chan)+"_"+str(self.main_km)+"_"+self.main_site, self.topo,0,5,0,10000,1]
+    # musi dziedziczyc razem z rzedem, narazie tylko stale domyslne
+    # main_chan, main_km, main_site ---- do nazewnictwa z cieku glownego
+    # topo bezposrednio z cieku glownego
+    element.data_definition()
+
 
 
 print(len(XS_dat))
@@ -76,4 +151,8 @@ print(XS_dat[1].max_left)
 print(XS_dat[1].max_right)
 print(XS_dat[1].len)
 print(len(linki))
-print(linki[0].river1," ", linki[0].river2)
+print(linki[0].definitions,"\n", linki[0].connections)
+print(linki[0].points,"\n", linki[0].geometry)
+print(linki[0].cross_section)
+
+
